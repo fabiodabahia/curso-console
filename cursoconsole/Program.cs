0b565dd9c17c9856bd9df12ab1c5a9d3a6567be4
fabiodabahia <fabio.da.bahia@hotmail.com>
2016-07-11 22:04:26 -0300

    commit com erro

--- cursoconsole/Program.cs
+++ cursoconsole/Program.cs
@@ -9,7 +9,71 @@ namespace cursoconsole
     class Program
     {
         static void Main(string[] args)
-        {
+        {int opcao, operacao;
+        string cliente = "", produto = "";
+        Console.Clear();
+        Console.WriteLine("1-Cliente");
+        Console.WriteLine("2-Produto");
+        Console.WriteLine("3-Sair");
+        opcao = Convert.ToInt32(Console.ReadLine());
+       
+        switch (opcao)
+            { case  1:
+                    Console.WriteLine("1- cadastrar Cliente");
+                    Console.WriteLine("2- Consultar Cliente");
+                    break;
+            case 2:
+                Console.WriteLine("1- Cadastrar Produto"); 
+                Console.WriteLine("2- Consultar Produto"); 
+                break;
+            case 3:
+                Console.WriteLine("Saindo..."); 
+                break;
+            default: Console.WriteLine("Operação Inválida!");
+                break;
+        }
+            if ((opcao.Equals(1)))
+            {
+                operacao = Convert.ToInt32(Console.ReadLine());
+                //if(operação.Equals(1))
+                
+                Console.Write("Informe o nome:");
+                cliente = Console.ReadLine();
+                //Interaction.MsgBox("Cliente cadastrado com sucesso!");
+            } 
+            else if ((operacao.Equals(2))) 
+            { 
+                Console.WriteLine("Nome do Cliente: " + cliente);
+                Console.ReadKey();
+            } 
+            else 
+            { 
+                Console.WriteLine("Opção Inválida!");
+            } 
+        } 
+        else if ((opcao.Equals(2)))
+    { 
+        operacao = Convert.ToInt32(Console.ReadLine());
+        if (operacao.Equals(1)) 
+    { 
+        Console.Write("Informe o nome:");
+        produto = Console.ReadLine();
+        //Interaction.MsgBox("Cliente cadastrado com sucesso!");
+    } 
+    else if ((operacao.Equals(2)))
+{ 
+    Console.Write("Descrição: " + produto);
+    Console.ReadKey();
+} 
+    else
+{ 
+    Console.WriteLine("Opção Inválida!");
+    } while (opcao != 3);
+} 
+}
+
+
+            }
         }
     }
 }
